c9d897cfddd12122da76a1a0ac4fc3b81de1d090
Arash <arashbi@gmail.com>
2025-04-04 11:44:48 -0400

    remove problematic includes

--- js/index.js
+++ js/index.js
@@ -30,15 +30,7 @@ if (!ilib._platform || (typeof(ilib._dyndata) !== 'boolean' && typeof(ilib._dync
             case 'nodejs':
                 require("./lib/ilib-node.js");
                 break;
-            case 'qt':
-                require("./lib/ilib-qt.js");
-                break;
-            case 'rhino':
-                require("./lib/ilib-rhino.js");
-                break;
-            case 'ringo':
-                require("./lib/ilib-ringo.js");
-                break;
+        
             default:
                 ilib._dyncode = false;
                 ilib._dyndata = false;
